afdd329f2ca4dda5f641548a0ad0b5316c514431
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-03-02 15:28:18 +0100

    Just removed SummaryWriter parts

--- LogKeyModel_train.py
+++ LogKeyModel_train.py
@@ -2,7 +2,7 @@ import time
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.tensorboard import SummaryWriter
+#from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import TensorDataset, DataLoader
 import argparse
 import os
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     model = Model(input_size, hidden_size, num_layers, num_classes).to(device)
     seq_dataset = generate('hdfs_train')
     dataloader = DataLoader(seq_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-    writer = SummaryWriter(log_dir='log/' + log)
+    #writer = SummaryWriter(log_dir='log/' + log)
 
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
@@ -87,13 +87,13 @@ if __name__ == '__main__':
             loss.backward()
             train_loss += loss.item()
             optimizer.step()
-            writer.add_graph(model, seq)
+            #writer.add_graph(model, seq)
         print('Epoch [{}/{}], train_loss: {:.4f}'.format(epoch + 1, num_epochs, train_loss / total_step))
-        writer.add_scalar('train_loss', train_loss / total_step, epoch + 1)
+        #writer.add_scalar('train_loss', train_loss / total_step, epoch + 1)
     elapsed_time = time.time() - start_time
     print('elapsed_time: {:.3f}s'.format(elapsed_time))
     if not os.path.isdir(model_dir):
         os.makedirs(model_dir)
     torch.save(model.state_dict(), model_dir + '/' + log + '.pt')
-    writer.close()
+    #writer.close()
     print('Finished Training')
