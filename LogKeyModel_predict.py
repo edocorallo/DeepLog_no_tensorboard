767cf3f6f5045fc1ccd2f9bbb4d001fee1a29587
edocorallounibo <58848670+edocorallounibo@users.noreply.github.com>
2020-10-01 18:48:42 +0200

    WIP
    WIP

--- LogKeyModel_predict.py
+++ LogKeyModel_predict.py
@@ -2,6 +2,39 @@ import torch
 import torch.nn as nn
 import time
 import argparse
+import importlib
+importlib.import_module('drain3')
+import configparser
+import logging
+import sys
+
+from drain3 import TemplateMiner
+from drain3.file_persistence import FilePersistence
+
+parser = argparse.ArgumentParser()
+group=parser.add_mutually_exclusive_group()
+group.add_argument("-f","--frontend",action="store_true",help="Used if you want to parse a storm-frontend log file.")
+group.add_argument("-b","--backend",action="store_true",help="Used if you want to parse a storm-backend log file.")
+parser.add_argument('-num_layers', default=2, type=int)
+parser.add_argument('-hidden_size', default=64, type=int)
+parser.add_argument('-window_size', default=10, type=int)
+parser.add_argument('-num_candidates', default=9, type=int)
+parser.add_argument('-log_file',default='hdfs',type=str)
+args = parser.parse_args()
+if args.frontend:
+        log_type="frontend-server"
+elif args.backend:
+        log_type="backend-server"
+persistence_type = "FILE"
+
+config = configparser.ConfigParser()
+config.read('drain3.ini')
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(message)s')
+
+persistence = FilePersistence("../Drain3/parser/results/{}/drain3_state[{}].bin".format(log_type,log_type))
+template_miner = TemplateMiner(persistence)
 
 # Device configuration
 device = torch.device("cpu")
@@ -41,15 +74,8 @@ class Model(nn.Module):
 if __name__ == '__main__':
 
     # Hyperparameters
-    num_classes = 28
+    num_classes = len(template_miner.drain.clusters)
     input_size = 1
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-log_file',default='hdfs',type=str)
-    parser.add_argument('-num_layers', default=2, type=int)
-    parser.add_argument('-hidden_size', default=64, type=int)
-    parser.add_argument('-window_size', default=10, type=int)
-    parser.add_argument('-num_candidates', default=9, type=int)
-    args = parser.parse_args()
     num_layers = args.num_layers
     hidden_size = args.hidden_size
     window_size = args.window_size
