1e7ff0caf363b90fea8db9de611f076de9288354
edocorallo <58848670+edocorallo@users.noreply.github.com>
2020-10-22 17:25:13 +0200

    Update LogKeyModel_train.py

--- LogKeyModel_train.py
+++ LogKeyModel_train.py
@@ -23,15 +23,6 @@ else:
 # Device configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def num_classes(name):
-    uniq=[]
-    with open('data/'+name,'r') as file:
-        for line in file:
-            line=line.strip().split()
-            for char in line:
-                if char not in uniq:
-                    uniq.append(char)
-    return len(uniq)
 
 def generate(name):
     num_sessions = 0
@@ -70,7 +61,7 @@ if __name__ == '__main__':
         
     log_file=args.log_file
     # Hyperparameters
-    num_classes = num_classes('{}_train'.format(log_file))
+    num_classes = 38
     print("num_classes :  ",num_classes)
     num_epochs = 300
     batch_size = 2048
